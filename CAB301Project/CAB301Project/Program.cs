19f4a22282113e17dbbe5325190dce2ff58e8f6e
Aiden <aidencran@gmail.com>
2022-06-07 12:09:18 +1000

    UserInterface Begin Implementation

--- CAB301Project/CAB301Project/Program.cs
+++ CAB301Project/CAB301Project/Program.cs
@@ -14,6 +14,11 @@ namespace CAB301Project
         static void Main(string[] args)
         {
             Program program = new Program();
+
+            program.communityLibrary.Insert(new Movie("A"));
+            program.communityLibrary.Insert(new Movie("B"));
+            program.communityLibrary.Insert(new Movie("C"));
+
             program.Run();
         }
 
@@ -22,9 +27,18 @@ namespace CAB301Project
         /// </summary>
         void Run()
         {
-            menu.Add("Register User", Display);
-            //menu.Add("Login User", () => checkLoginUser());
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Welcome to Community Library Movie DVD Management System");
+            Console.WriteLine("======================================================");
+
+            Console.WriteLine("====================Main Menu=======================");
+
+            menu.Add("Staff Login", DisplayStaffMember);
+            menu.Add("Member Login", DisplayMember);
             menu.Add("Exit", () => Environment.Exit(0));
+
+            menu.Add("Display All Movies", DisplayAllDVDs);
+            menu.Add("Display Movie Info", DisplayMovieInfo);
             DisplayMainMenu();
         }
 
@@ -37,110 +51,78 @@ namespace CAB301Project
                 menu.Display();
         }
 
-
-        public void Display()
-        {
-            Console.WriteLine("======================================================");
-            Console.WriteLine("Welcome to Community Library Movie DVD Management System");
-            Console.WriteLine("======================================================");
-
-            Console.WriteLine("====================Main Menu=======================");
-
-            Console.WriteLine("1) Staff Login");
-            Console.WriteLine("2) Member Login");
-            Console.WriteLine("0) Exit");
-
-            //log in controller
-            LoginController();
-            
-        }
-
         public void DisplayStaffMember()
         {
-            Console.WriteLine("====================Staff Menu=======================");
-            
-            
-            Console.WriteLine("1) Add new DVDs of new movie to the system");
-            Console.WriteLine("2) Remove DVDS of a movie from the system");
-            Console.WriteLine("3) Register a new member with the system");
-            Console.WriteLine("4) Remove a registered member from the system");
-            Console.WriteLine("5) Display a member's contact phone number, given the member's name");
-            Console.WriteLine("6) Display all members who are currently renting a particuler movie");
-            Console.WriteLine("0) Return to the main menu");
-            
+            Menu submenu = new Menu();
+
+            UserInterface.Message("====================Staff Menu=======================");
+
+            submenu.Add("Add new DVDs of new movie to the system", menu.Display);
+            submenu.Add("Remove DVDS of a movie from the system", menu.Display);
+            submenu.Add("Register a new member with the system", menu.Display);
+            submenu.Add("Remove a registered member from the system", menu.Display);
+            submenu.Add("Display a member's contact phone number, given the member's name", menu.Display);
+            submenu.Add("Display all members who are currently renting a particuler movie", menu.Display);
+            submenu.Add("Return to the main menu", menu.Display);
+
+            submenu.Display();
         }
 
         public void DisplayMember()
         {
-            Console.WriteLine("====================Member Menu=======================");
+            Menu submenu = new Menu();
 
+            UserInterface.Message("====================Member Menu=======================");
 
-            Console.WriteLine("1) Browse all the movies");
-            Console.WriteLine("2) Display all the information about a movie, given the title of the movie");
-            Console.WriteLine("3) Borrow a movie DVD");
-            Console.WriteLine("4) Return a movie DVD");
-            Console.WriteLine("5) List current borrowing movies");
-            Console.WriteLine("6) Display the top 3 movies rented by the members");
-            Console.WriteLine("0) Return to the main menu");
+            submenu.Add("Browse all the movies", menu.Display);
+            submenu.Add("Display all the information about a movie, given the title of the movie", menu.Display);
+            submenu.Add("Borrow a movie DVD", menu.Display);
+            submenu.Add("Return a movie DVD", menu.Display);
+            submenu.Add("List current borrowing movies", menu.Display);
+            submenu.Add("Display the top 3 movies rented by the members", menu.Display);
+            submenu.Add("Return to the main menu", menu.Display);
 
-            
+            submenu.Display();
         }
 
-        public void LoginController()
-        {
-            string userInputext = Console.ReadLine();
-            int userinput = GetInt(userInputext,0 ,2);
+        #region To Be Removed
+
+        //public void StaffController()
+        //{
+        //    DisplayStaffMember();
+
+        //    string userInputText = Console.ReadLine();
+        //    int userInput = GetInt(userInputText,0 ,6);
 
 
-            switch (userinput)
-            {
-                case 1:
-                    StaffController();
-                    break;
-                case 2:
+        //    switch (userInput)
+        //    {
+        //        case 1:
+        //            AddMovie(communityLibrary);
+        //            DisplayStaffMember();
+        //            StaffController();
+        //            break;
+        //        case 2:
+        //            RemoveMovie(communityLibrary);
+        //            break;
+        //        case 3:
 
-                case 0:
+        //        case 4:
 
-                default:
-                    break;
-            }
-        }
+        //        case 5:
 
-        public void StaffController()
-        {
-            DisplayStaffMember();
+        //        case 6:
 
-            string userInputText = Console.ReadLine();
-            int userInput = GetInt(userInputText,0 ,6);
-
-
-            switch (userInput)
-            {
-                case 1:
-                    AddMovie(communityLibrary);
-                    DisplayStaffMember();
-                    StaffController();
-                    break;
-                case 2:
-                    RemoveMovie(communityLibrary);
-                    break;
-                case 3:
-
-                case 4:
-
-                case 5:
-
-                case 6:
-
-                case 0:
+        //        case 0:
 
 
-                default:
-                    break;
-            }
+        //        default:
+        //            break;
+        //    }
 
-        }
+        //}
 
+        #endregion
 
         #region StaffMember
         public void AddMovie(MovieCollection movieCollection)
@@ -232,7 +214,7 @@ namespace CAB301Project
             else if (movie == null)
             {
                 Console.WriteLine("Movie Does not exists");
-                StaffController();
+                //StaffController();
             }
 
             if (movie.AvailableCopies == 0)
@@ -292,7 +274,12 @@ namespace CAB301Project
 
         void DisplayAllDVDs()
         {
+            Menu submenu = new Menu();
             StringBuilder output = new StringBuilder();
+
+            Console.Clear();
+
+            output.AppendLine("All Catalogued DVDs in Alphabetical Order\n");
 
             foreach (Movie movie in communityLibrary.ToArray())
             {
@@ -301,30 +288,33 @@ namespace CAB301Project
 
             Console.WriteLine(output.ToString());
 
-            // Go Somewhere - E.g. Display.
-            DisplayMember();
+            // Return To Menu
+            menu.Display();
         }
 
         void DisplayMovieInfo()
         {
+            Menu submenu = new Menu();
+            Console.Clear();
+
+            UserInterface.Message("Please Insert a Movie Title");
+
             string title = Console.ReadLine();
             IMovie movie = communityLibrary.Search(title);
 
             if (movie == null)
             {
-                Console.WriteLine("Movie not found!\n1) Retry\n2)Return");
-                int input = GetInt(Console.ReadLine(), 1, 2);
-                if (input == 1) { DisplayMovieInfo(); return; }
-                // Else
-                // More ideal to call previous display rather than first
-                DisplayMember();
-                return;
+                UserInterface.Error("Movie not found!");
+
+                submenu.Add("Retry", DisplayMovieInfo);
+                submenu.Add("Return", menu.Display);
+                submenu.Display();
             }
-
-            Console.WriteLine(movie.ToString());
-
-            // Go Somewhere - E.g. Display
-            DisplayMember();
+            else
+            {
+                Console.WriteLine(movie.ToString());
+                menu.Display();
+            }
         }
 
         #endregion
